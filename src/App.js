c4492109ef0b8650a168c30a2b6241812d707c89
Kingson.Arum <nwajiudo@gmail.com>
2024-01-26 22:11:29 +0100

    setup folders

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from "react"
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
+    <div>
+     <Navbar />
     </div>
   );
 }
-
 export default App;
