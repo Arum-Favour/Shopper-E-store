f9d08f9b8f3b75669bab8b2feac1a2373559e040
Kingson.Arum <nwajiudo@gmail.com>
2024-01-29 23:36:20 +0100

    commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Hero.css";
 import hand_icon from "../Assets/hand_icon.png";
 import arrow_icon from "../Assets/arrow.png"
+import hero_image from "../Assets/hero_image.png"
 
 export const Hero = () => {
   return (
@@ -9,7 +10,7 @@ export const Hero = () => {
       <div className="hero-left">
         <h2>NEW ARRIVALS ONLY</h2>
         <div>
-          <div className="hand-hand-icon">
+          <div className="hero-hand-icon">
             <p>new</p>
             <img src={hand_icon} alt="" />
           </div>
@@ -18,10 +19,12 @@ export const Hero = () => {
         </div>
         <div className="hero-latest-btn">
           <div>Latest Collection</div>
-          <img src={arrow_icon} alt=""/>
+          <img src={arrow_icon} alt="arrow icon"/>
         </div>
       </div>
-      <div className="hero-right"></div>
+      <div className="hero-right">
+        <img  src={hero_image} alt=""/>
+      </div>
     </div>
   );
 };
