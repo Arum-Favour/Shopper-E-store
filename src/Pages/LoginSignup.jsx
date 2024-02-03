7f37e34a98275907db8eaca1ece13de1567ff83d
Kingson.Arum <nwajiudo@gmail.com>
2024-02-03 23:02:04 +0100

    commit

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,18 +1,27 @@
 import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
-  return <div className="loginSignup">
-    <div className="loginSignup-container">
-      <h1>Sign Up</h1>
-      <div className="loginsignup-fields">
-        <input type="text" placeholder="Your Name"/>
-        <input type="email" placeholder="Email Address"/>
-        <input type="password" placeholder="Password"/>
+  return (
+    <div className="loginSignup">
+      <div className="loginSignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id=""/>
+          <p>By continuing I agree to the terms of use and privacy</p>
+        </div>
       </div>
-      <button>Continue</button>
-      <p className="loginsignup-login">Already have an account?<span>Login Here</span></p>
     </div>
-  </div>;
+  );
 };
 
 export default LoginSignup;
