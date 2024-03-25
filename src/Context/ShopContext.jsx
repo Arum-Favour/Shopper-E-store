4faeabb93d27b9b1e63f56bbc361d44eb9e1532d
Kingson.Arum <nwajiudo@gmail.com>
2024-03-25 19:31:16 +0100

    Some changes

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -15,7 +15,7 @@ export const ShopContextProvider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   const addToCart = (itemId) => {
-    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 })); 
     console.log(cartItems);
   };
 
